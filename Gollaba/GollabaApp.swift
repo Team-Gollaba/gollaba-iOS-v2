1bbfc243494f12dac5f70339f8c7d4f09c0194b1
kimkihyeon <kimki1125@naver.com>
2024-11-08 19:52:04 +0900

    Initial Commit

---
+++ Gollaba/GollabaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GollabaApp.swift
+//  Gollaba
+//
+//  Created by 김견 on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GollabaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
