cc88917f6e45b004a3ee6b4ec90c0e4743bf4939
kimkihyeon <kimki1125@naver.com>
2024-11-08 21:09:02 +0900

    [fix] 폰트 이름 오타 변경

--- Gollaba/Extension/Font+.swift
+++ Gollaba/Extension/Font+.swift
@@ -9,13 +9,13 @@ import Foundation
 import SwiftUI
 
 extension Font {
-    static let dungGeunMo12: Font = .custom("SUIT-Variable", size: 12)
-    static let dungGeunMo16: Font = .custom("SUIT-Variable", size: 16)
-    static let dungGeunMo20: Font = .custom("SUIT-Variable", size: 20)
-    static let dungGeunMo24: Font = .custom("SUIT-Variable", size: 24)
-    static let dungGeunMo30: Font = .custom("SUIT-Variable", size: 30)
-    static let dungGeunMo32: Font = .custom("SUIT-Variable", size: 32)
-    static let dungGeunMo80: Font = .custom("SUIT-Variable", size: 80)
+    static let suit_variable12: Font = .custom("SUIT-Variable", size: 12)
+    static let suit_variable16: Font = .custom("SUIT-Variable", size: 16)
+    static let suit_variable20: Font = .custom("SUIT-Variable", size: 20)
+    static let suit_variable24: Font = .custom("SUIT-Variable", size: 24)
+    static let suit_variable30: Font = .custom("SUIT-Variable", size: 30)
+    static let suit_variable32: Font = .custom("SUIT-Variable", size: 32)
+    static let suit_variable80: Font = .custom("SUIT-Variable", size: 80)
     
     static let yangjin30: Font = .custom("YANGJIN", size: 30)
 }
