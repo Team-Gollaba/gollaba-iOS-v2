fc31ef58ae2cb31a6f24d26f286ac313df3ab5d5
kimkihyeon <kimki1125@naver.com>
2024-11-08 21:27:24 +0900

    [feat] 탭뷰로 화면 나누어 구성.

--- Gollaba/HomeView.swift
+++ Gollaba/HomeView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var tabIndex: Int = 0
+    
     var body: some View {
         VStack {
             HStack {
@@ -17,11 +19,43 @@ struct HomeView: View {
                 
                 Text("골라바")
                     .font(.yangjin30)
-                    .underline(color: Color.titleFontColor)
+                    .underline(color: .titleFontColor)
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("로그인")
+                        .font(.suit_variable16)
+                        .foregroundStyle(.black)
+                }
             }
             .frame(maxWidth: .infinity)
             .frame(height: 80)
+            .padding(.horizontal)
             .background(Color.toolbarBackgroundColor)
+            
+            TabView(selection: $tabIndex) {
+                Text("홈")
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                    }
+                    .tag(0)
+                
+                Text("만들기")
+                    .tabItem {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .tag(0)
+                
+                Text("내 투표")
+                    .tabItem {
+                        Image(systemName: "folder.fill")
+                    }
+                    .tag(0)
+            }
+            .tint(.black)
         }
     }
 }
