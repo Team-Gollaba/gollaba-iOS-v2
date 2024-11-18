dbd423d3b4362f7a3ae31597d8caa46dfa61b352
kimkihyeon <kimki1125@naver.com>
2024-11-18 16:58:00 +0900

    [feat] 투표 생성 화면 스크롤뷰로 변경

--- Gollaba/View/CreatePoll/CreatePollView.swift
+++ Gollaba/View/CreatePoll/CreatePollView.swift
@@ -17,71 +17,73 @@ struct CreatePollView: View {
     @State var isQuestionPresent: Bool = false
     
     var body: some View {
-        VStack (spacing: 16) {
-            OptionBoxView(title: "투표 제목") {
-                TextField("투표의 제목을 입력해주세요.", text: $text)
+        ScrollView {
+            VStack (spacing: 16) {
+                OptionBoxView(title: "투표 제목") {
+                    TextField("투표의 제목을 입력해주세요.", text: $text)
+                        .font(.suitVariable16)
+                }
+                
+                ThreeByTwoGrid(pollItemName: $pollItemName)
+                
+                
+                HStack (spacing: 20) {
+                    
+                    OptionBoxView(title: "익명 여부") {
+                        ChooseTwoOptionsView(
+                            selectedOption: $anonymousOption,
+                            firstOptionText: "익명 투표",
+                            firstOptionImage: Image("AnonymousIcon"),
+                            secondOptionText: "기명 투표",
+                            secondOptionImage: Image("SignIcon")
+                        )
+                    }
+                    
+                    OptionBoxView(title: "투표 가능 옵션 개수") {
+                        ChooseTwoOptionsView(
+                            selectedOption: $countingOption,
+                            firstOptionText: "단일 투표",
+                            firstOptionImage: Image("OnlyPollIcon"),
+                            secondOptionText: "복수 투표",
+                            secondOptionImage: Image("PluralIcon")
+                        )
+                    }
+                }
+                
+                
+                OptionBoxView(title: "투표 종료 기간") {
+                    CallendarOptionView()
+                }
+                
+                QuestionButton {
+                    isQuestionPresent = true
+                }
+                
+                EnrollPollButton {
+                    
+                }
+                
+            }
+            .padding(.horizontal)
+            .dialog(
+                isPresented: $isQuestionPresent,
+                title: "투표 만들기 도움말",
+                content: Text("기명투표")
+                    .font(.suitBold16)
+                + Text("를 선택하여 로그인 된 사용자의 투표만 받을 수 있어요.")
                     .font(.suitVariable16)
-            }
-            
-            ThreeByTwoGrid(pollItemName: $pollItemName)
-            
-            
-            HStack (spacing: 20) {
-                
-                OptionBoxView(title: "익명 여부") {
-                    ChooseTwoOptionsView(
-                        selectedOption: $anonymousOption,
-                        firstOptionText: "익명 투표",
-                        firstOptionImage: Image("AnonymousIcon"),
-                        secondOptionText: "기명 투표",
-                        secondOptionImage: Image("SignIcon")
-                    )
-                }
-                
-                OptionBoxView(title: "투표 가능 옵션 개수") {
-                    ChooseTwoOptionsView(
-                        selectedOption: $countingOption,
-                        firstOptionText: "단일 투표",
-                        firstOptionImage: Image("OnlyPollIcon"),
-                        secondOptionText: "복수 투표",
-                        secondOptionImage: Image("PluralIcon")
-                    )
-                }
-            }
-            
-            
-            OptionBoxView(title: "투표 종료 기간") {
-                CallendarOptionView()
-            }
-            
-            QuestionButton {
-                isQuestionPresent = true
-            }
-            
-            EnrollPollButton {
-                
-            }
-            
+                + Text("\n\n복수투표")
+                    .font(.suitBold16)
+                + Text("로 여러 선택지를 동시에 고르도록 할 수 있어요.")
+                    .font(.suitVariable16)
+                + Text("\n\n마감일")
+                    .font(.suitBold16)
+                + Text("을 설정해 투표 기간을 원하는 만큼 지정할 수 있어요.")
+                    .font(.suitVariable16),
+                primaryButtonText: "확인",
+                onPrimaryButton: {}
+            )
         }
-        .padding(.horizontal)
-        .dialog(
-            isPresented: $isQuestionPresent,
-            title: "투표 만들기 도움말",
-            content: Text("기명투표")
-                .font(.suitBold16)
-            + Text("를 선택하여 로그인 된 사용자의 투표만 받을 수 있어요.")
-                .font(.suitVariable16)
-            + Text("\n\n복수투표")
-                .font(.suitBold16)
-            + Text("로 여러 선택지를 동시에 고르도록 할 수 있어요.")
-                .font(.suitVariable16)
-            + Text("\n\n마감일")
-                .font(.suitBold16)
-            + Text("을 설정해 투표 기간을 원하는 만큼 지정할 수 있어요.")
-                .font(.suitVariable16),
-            primaryButtonText: "확인",
-            onPrimaryButton: {}
-        )
         
     }
 }
