dc810739ab2c9ade2e30f86cf965d4eed2a6596b
kimkihyeon <kimki1125@naver.com>
2024-11-24 17:44:48 +0900

    [chore] My투표 화면 스크롤뷰 뺌

--- Gollaba/View/MyPoll/MyPollView.swift
+++ Gollaba/View/MyPoll/MyPollView.swift
@@ -11,61 +11,53 @@ struct MyPollView: View {
     @State var viewModel = MyPollViewModel()
     
     var body: some View {
-        ScrollView {
-            ZStack {
-                VStack (spacing: 12) {
-                    
-                    ProfileImageView(image: Image("cha_eun_woo")) {
-                        viewModel.isClickedProfileImage = true
-                    }
-                    
-                    ProfileNameView(name: "Cha eunwoo")
-                    
-//                    MyLikePollsCountView(countLikePolls: 42, action: {
-//                        
-//                    })
-                    
-                    GoToMyPollListButton(
-                        icon: Image(systemName: "person"),
-                        title: "내가 만든 투표",
-                        action: {
-                            viewModel.pollListIcon = Image(systemName: "person")
-                            viewModel.pollListTitle = "내가 만든 투표"
-                            viewModel.goToPollList = true
-                        }
-                    )
-                    
-                    Divider()
-                        .padding(.horizontal)
-                    
-                    GoToMyPollListButton(
-                        icon: Image(systemName: "heart.fill"),
-                        title: "내가 좋아요한 투표",
-                        action: {
-                            viewModel.pollListIcon = Image(systemName: "heart.fill")
-                            viewModel.pollListTitle = "내가 좋아요한 투표"
-                            viewModel.goToPollList = true
-                        }
-                    )
-                    
-//                    VerticalPollList(
-//                        goToPollDetail: $viewModel.goToPollDetail,
-//                        icon: Image(systemName: "person"),
-//                        title: "내가 만든 투표"
-//                    )
-                    
-                    LogoutButton {
-                        
-                    }
-                    .padding(.top)
+        
+        VStack (spacing: 12) {
+            
+            ProfileImageView(image: Image("cha_eun_woo")) {
+                viewModel.isClickedProfileImage = true
+            }
+            
+            ProfileNameView(name: "Cha eunwoo")
+            
+            //                    MyLikePollsCountView(countLikePolls: 42, action: {
+            //
+            //                    })
+            
+            
+            GoToMyPollListButton(
+                icon: Image(systemName: "person"),
+                title: "내가 만든 투표",
+                action: {
+                    viewModel.pollListIcon = Image(systemName: "person")
+                    viewModel.pollListTitle = "내가 만든 투표"
+                    viewModel.goToPollList = true
                 }
-                .padding(.vertical)
-                
+            )
+            
+            Divider()
+                .padding(.horizontal)
+            
+            GoToMyPollListButton(
+                icon: Image(systemName: "heart.fill"),
+                title: "내가 좋아요한 투표",
+                action: {
+                    viewModel.pollListIcon = Image(systemName: "heart.fill")
+                    viewModel.pollListTitle = "내가 좋아요한 투표"
+                    viewModel.goToPollList = true
+                }
+            )
+            
+            Spacer()
+            
+            LogoutButton {
                 
             }
-            .sheet(isPresented: $viewModel.isClickedProfileImage) {
-                ProfileImageDetailView(image: Image("cha_eun_woo"))
-            }
+            .padding(.top)
+        }
+        .padding(.vertical)
+        .sheet(isPresented: $viewModel.isClickedProfileImage) {
+            ProfileImageDetailView(image: Image("cha_eun_woo"))
         }
         .navigationDestination(isPresented: $viewModel.goToPollDetail) {
             PollDetailView()
@@ -75,6 +67,8 @@ struct MyPollView: View {
                 MyPollListView(icon: icon, title: title)
             }
         }
+        
+        
     }
 }
 
